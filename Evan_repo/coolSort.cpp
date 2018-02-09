558b85d5de051bc3a8142d865be502ec824e8422
Joel Zarley <joelzarley@aol.com>
2018-02-09 15:43:35 -0800

    added Print function

--- Evan_repo/coolSort.cpp
+++ Evan_repo/coolSort.cpp
@@ -3,12 +3,22 @@
 
 using namespace std;
 
+void printArr(int arr[], int size){
+  cout << "\n** Printing Arr **" << endl;
+  for(int i=0; i<size; i++){
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  return;
+}
+
 int* CoolSort(int unsortedArr[], int arrSize, int H[], int hSize)
 {
   //Instantiate and Assign Variables
   int* sortedArr = unsortedArr;
   int acc = 0;
-  int a, b;
+  int a, b, tmp;
 
   //Begin sorting
   while(acc < hSize){
@@ -25,16 +35,22 @@ int* CoolSort(int unsortedArr[], int arrSize, int H[], int hSize)
     for(int i=0; i<stepSize; i++){
 	a = i;
 	b = a + stepSize;
-	
-	cout << sortedArr[a] << " ";;
+
+	if(sortedArr[a] > sortedArr[b]){
+	  tmp = sortedArr[a];
+	  sortedArr[a] = sortedArr[b];
+	  sortedArr[b] = tmp;
+	}
+	cout << sortedArr[a] << " ";
 	while(b < arrSize){
 	  cout << sortedArr[b] << " ";
 	  b += stepSize;
 	}
-	
 	cout << endl;
-      }
-    
+    }
+
+    printArr(sortedArr, arrSize);
+
     acc++;
   }
 
--- Evan_repo/coolSort.h
+++ Evan_repo/coolSort.h
@@ -1,6 +1,7 @@
 #ifndef COOLSORT_H
 #define COOLSORT_H
 
+void printArr(int[], int);
 int* CoolSort(int[], int, int[], int);
 
 #endif
